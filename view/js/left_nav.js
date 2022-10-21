9eb1dacf0eaf22136484bda3978229232d11bcef
thejanaakmeemana <thejanaakmeemana@gmail.com>
2022-10-21 16:17:49 +0530

    merchandiser first commit

---
+++ view/js/left_nav.js
@@ -0,0 +1,60 @@
+
+  
+    function displayLeftNav() {
+      var x = document.getElementById("left-nav");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+        window.scrollTo(0,0);
+      } else {
+        x.style.display = "none";
+        window.scrollTo(0,0);
+      }
+    } 
+
+    function displayLeftNavSub1() {
+      var x = document.getElementById("left-nav-sub-1");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    } 
+    function displayLeftNavSub2() {
+      var x = document.getElementById("left-nav-sub-2");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    } 
+    function displayLeftNavSub3() {
+      var x = document.getElementById("left-nav-sub-3");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    } 
+    function displayLeftNavSub4() {
+      var x = document.getElementById("left-nav-sub-4");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    } 
+    function displayLeftNavSub5() {
+      var x = document.getElementById("left-nav-sub-5");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    } 
+
+    /*window.addEventListener('resize', function(event){
+      var boxHeight = document.getElementById('form-box').style.height;
+      document.getElementById('left-orange-box').style.height = boxHeight;
+    });  */
+    
+  
---
+++ view/merchandiser/add_costume_order_onsite.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>View costume order</title>
+        <link rel="stylesheet" type="text/css" href="../css/merchandiser/data_form_style.css" />
+    </head>
+
+    <body>
+        <?php include 'header.php';?>
+
+        <div id="page-body">
+            <?php include 'leftnav.php';?>
+
+            <div id="page-content">
+                <div id="breadcrumb">
+                    <a href="#">Welcome </a> >
+                    <a href="#">Login </a> >
+                    <a href="#">Merchandiser </a> >
+                    <a href="#">Costume orders </a> > View
+                </div>
+
+                <div id="form-box">
+                    <form method="post" action="">
+                        <center>
+                            <h2>Add costume order(onsite)</h2>
+                        </center>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Order ID : 
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="order_id" id="order_id" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Quotation ID : 
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="order_id" id="order_id" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Customer ID : 
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="order_id" id="order_id" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Customer name : 
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="order_id" id="order_id" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Customer contact no : 
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="order_id" id="order_id" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Customer email : 
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="order_id" id="order_id" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                <b>Design name</b>
+                            </div>
+                            <div class="form-row-data">
+                                <span><b>Quantity</b></span>
+                                <span><b>Unit price(LKR)</b></span>
+                                <span><b>Price(LKR)</b></span>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Black T-shirt-small
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="" id="" class="column-textfield" />
+                                <input type="text" name="" id="" class="column-textfield" />
+                                <input type="text" name="" id="" class="column-textfield" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Black T-shirt-large
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="" id="" class="column-textfield" />
+                                <input type="text" name="" id="" class="column-textfield" />
+                                <input type="text" name="" id="" class="column-textfield" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Black T-shirt-XXL
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="" id="" class="column-textfield" />
+                                <input type="text" name="" id="" class="column-textfield" />
+                                <input type="text" name="" id="" class="column-textfield" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Total items :
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="" id="" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Total price (LKR) :
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="" id="" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Expected delivery date :
+                            </div>
+                            <div class="form-row-data">
+                                <input type="date" name="" id="" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                <b>Advance payment (LKR) :</b>
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="" id="" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Quotation issued on :
+                            </div>
+                            <div class="form-row-data">
+                                <input type="date" name="" id="" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Quotation valid till :
+                            </div>
+                            <div class="form-row-data">
+                                <input type="date" name="" id="" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Acceptance (By merchandiser) :
+                            </div>
+                            <div class="form-row-data">
+                                <table width="60%">
+                                    <tr>
+                                        <td>
+                                            <input type="radio" name="acceptance" class="input-radio" id="" /> Accept
+                                        </td>
+                                        <td>
+                                            <input type="radio" name="acceptance" class="input-radio" id="" /> Reject
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Acceptance description :
+                            </div>
+                            <div class="form-row-data">
+                                <textarea id="" name="" rows="4" cols="40"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Product status :
+                            </div>
+                            <div class="form-row-data">
+                                <table width="60%">
+                                    <tr>
+                                        <td>
+                                            <input type="radio" name="product_status" class="input-radio" id="" /> Incomplete
+                                        </td>
+                                        <td>
+                                            <input type="radio" name="product_status" class="input-radio" id="" /> Complete
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Quality (By manager) :
+                            </div>
+                            <div class="form-row-data">
+                                <table width="60%">
+                                    <tr>
+                                        <td>
+                                            <input type="radio" name="quality" class="input-radio" id="" disabled /> Good
+                                        </td>
+                                        <td>
+                                            <input type="radio" name="quality" class="input-radio" id="" disabled /> Bad
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Quality description :
+                            </div>
+                            <div class="form-row-data">
+                                <textarea id="" name="" rows="4" cols="40" disabled></textarea>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                <b>Balance payment (LKR) :</b>
+                            </div>
+                            <div class="form-row-data">
+                                <input type="text" name="" id="" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-theme">
+                                Delivered date :
+                            </div>
+                            <div class="form-row-data">
+                                <input type="date" name="" id="" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-row-submit">
+                                <input type="submit" value="Confirm editions" />
+                            </div>
+                            <div class="form-row-reset">
+                                <input type="reset" value="Cancel" />
+                            </div>
+                        </div> 
+                    </form>
+                </div>   
+            </div> 
+        </div> 
+
+        <?php include 'footer.php';?>
+
+    </body> 
+</html>
