0b49ba30959107c29f0b8c6c989436ba7ba1b821
Diani Dickovita <108466170+DianiDickovita@users.noreply.github.com>
2023-03-17 21:51:04 +0530

    customer commit 3/17 2

--- view/customer/footer.php
+++ view/customer/footer.php
@@ -1,4 +1,34 @@
-        <link rel="stylesheet" type="text/css" href="../css/merchandiser/footer_style.css" />
+<?php
+    require_once('../../model/database.php');
+    $conn = mysqli_connect($db_params['servername'], $db_params['username'], $db_params['password'], $db_params['dbname']);
+    if($conn->connect_error){
+        die("Connection Faild: ". $conn->connect_error);
+    }
+
+    if(isset($_POST["sendMessage"])){
+        $sql = "INSERT INTO feedback (name, email, message) VALUES (?,?,?);";
+        if ($stmt = mysqli_prepare($conn, $sql)) {
+            mysqli_stmt_bind_param($stmt, "sss", $_POST["name"], $_POST["email"], $_POST["message"]);
+            mysqli_stmt_execute($stmt);
+            $feedbackID = $conn->insert_id;
+            if($feedbackID == 0){
+                ?><script>alert("Sorry! Message wasn't sent");
+                    window.location.href='<?php echo $_POST["page_url"]; ?>';
+                </script><?php   
+            }else{
+                ?><script>alert("Message was sent successfully");
+                    window.location.href='<?php echo $_POST["home_url"]; ?>';
+                </script><?php             
+            }
+            $stmt->close(); 
+        } else {
+            echo "Error: <br>" . mysqli_error($conn);
+        } 
+        $conn->close();
+    }
+    
+?>
+       <link rel="stylesheet" type="text/css" href="../css/merchandiser/footer_style.css" />
         
         <div id="page-footer">
             <hr color="#cccccc" size="8px" width="100%" style="margin:0;" /> 
@@ -21,11 +51,13 @@
             </div>
             <div id="footer-right-column">
                 <b>Your message</b><br /><br />
-                <form method="post" action="">
+                <form method="post" action="footer.php">
+                    <input type="text" hidden="true" name="page_url" value="<?php echo $_SERVER['REQUEST_URI']; ?>" />
+                    <input type="text" hidden="true" name="home_url" value="customer_home.php" />
                     <input type="text" name="name" width="30%" placeholder="Name" /><br />
                     <input type="text" name="email" width="30%" placeholder="Email" /><br />
                     <textarea name="message" rows="4" cols="30" placeholder="Message"></textarea><br />
-                    <input type="submit" value="Send" />
+                    <input type="submit" name="sendMessage" value="Send" />
                 </form>
             </div> 
         </div>
