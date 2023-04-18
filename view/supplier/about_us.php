c86cc34cd07dcfd065c2725da8ee407ce650498d
Nayomi17 <108719172+Nayomi17@users.noreply.github.com>
2023-04-18 23:06:51 +0530

    supplier_commit-4-18

--- view/supplier/about_us.php
+++ view/supplier/about_us.php
@@ -6,6 +6,24 @@
         <title>About Us</title>
         <link rel="stylesheet" type="text/css" href="../supplier/css/data_form_style.css" />
         <link rel="stylesheet" type="text/css" href="../supplier/css/view_list_style.css" />
+        <style>
+        p {
+  display: block;
+  margin-top: 2em;
+  margin-bottom: 1em;
+  margin-left: 5em;
+  margin-right: 0;
+  text-align: left;
+  
+}
+
+h2{
+    margin-top: 3em;
+    margin-left: 3em;
+    text-align: left;
+    color:#330099;
+}
+</style>
     </head>
 
     <body>
@@ -16,15 +34,13 @@
 
             <div id="page-content">
                 <div id="breadcrumb">
-                    <a href="index.php">Welcome </a> >
-                     About Us
+                    <a href="index.php">Welcome </a> 
+                     > About us
                 </div>
                 
-                <div id="list-box-small">
+                <div id="list-box-small" style ="background-image: url(images/about_us.jpg); height: 400px; width: 1000px;  background-repeat: no-repeat;">
                     <center>
                         <h2>About Us</h2>
-                    
-
                     <p>RLF Apparel T-shirt manufacturing <br> company started in 2012. Mr.Anoona <br> Udattawa is a product development <br> technologist who works in RLF apparel <br> factory. He was graduate in University of <br>moratuwa. And he has a degree in <br> Bachelor of Design in Fashion Design & <br> Product Development, University of <br> Moratuwa .</p>
                     </center>
                 </div>
