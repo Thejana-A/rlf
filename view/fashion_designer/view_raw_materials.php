0e1102860cf449f6885abf8b4a82e0a26af427e7
Kaveesha Nethmini <108716880+kaveenethmini@users.noreply.github.com>
2023-05-08 15:56:58 +0530

    fd_5/8

--- view/fashion_designer/view_raw_materials.php
+++ view/fashion_designer/view_raw_materials.php
@@ -6,7 +6,65 @@
               <title> View Raw materials </title>
               <link rel="stylesheet" type="text/css" href="../css/fashion_designer/data_form_style.css" />
               <link rel="stylesheet" type="text/css" href="../css/fashion_designer/view_list_style.css" />
+              <?php 
+                    require_once('../../model/DBConnection.php');
+                    $connObj = new DBConnection();
+                    $conn = $connObj->getConnection();
+                    if(isset($_POST["search"])){
+                       $searchbar = $_POST["searchbar"];
+                        $search_sql = "SELECT material_id, name, measuring_unit, quantity_in_stock FROM raw_material WHERE material_id LIKE '%$searchbar%' OR name LIKE '%$searchbar%' OR measuring_unit LIKE '%$searchbar%' OR quantity_in_stock LIKE '%$searchbar%'";
+                        $serach_output = "";
+                        $output = "";
+                        if($search_result = mysqli_query($conn, $search_sql)){
+                            if(mysqli_num_rows($search_result) > 0){
+                                while($search_row = mysqli_fetch_array($search_result)){
+                                    $search_output.= "<div class='item-data-row'>";
+                                    $search_output.= "<form method='post' action='../RouteHandler.php'>";
+                                    $search_output.= "<input type='text' hidden='true' name='framework_controller' value='raw_material/fashion_designer_view' />";
+                                    $search_output.= "<input type='text' hidden='true' name='material_id' value='".$search_row["material_id"]."' />";
+                                    $search_output.= "<span class='manager-ID-column'>".$search_row["material_id"]."</span><span>".$search_row["name"]."</span><span style='padding-left:24px;'>".$search_row["measuring_unit"]."</span>";
+                                    $search_output.= "<table align='right' style='margin-right:8px;' class='two-button-table'><tr>";
+                                    $search_output.= "<td><input type='submit' class='grey' value='View' /></td>";
+                                    $search_output.= "</tr></table>"; 
+                                
+                                    $search_output.= "<hr class='manager-long-hr' />";
+                                    $search_output.= "</form>";
+                                    $search_output.= "</div>";
+                                }
+                            }else {
+                                    $search_output.= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No results found";
+                                    }
+                        }
+                    }else{ 
+                        $sql = "SELECT material_id, name, measuring_unit, quantity_in_stock FROM `raw_material` WHERE `manager_approval`='approve';";
+                        $serach_output = "";
+                        $output = "";
+                        if($result = mysqli_query($conn, $sql)){
+                            if(mysqli_num_rows($result) > 0){
+                                while($row = mysqli_fetch_array($result)){
+                                    //$class = ($row["manager_approval"]=="approve"):"grey";
+                                    $output.= "<div class='item-data-row'>";
+                                    $output.= "<form method='post' action='../RouteHandler.php'>";
+                                    $output.= "<input type='text' hidden='true' name='framework_controller' value='raw_material/fashion_designer_view' />";
+                                    $output.= "<input type='text' hidden='true' name='material_id' value='".$row["material_id"]."' />";
+                                    $output.= "<span class='manager-ID-column'>".$row["material_id"]."</span><span>".$row["name"]."</span><span style='padding-left:24px;'>".$row["measuring_unit"]."</span>";
+                                    $output.= "<table align='right' style='margin-right:8px;' class='two-button-table'><tr>";
+                                    $output.= "<td><input type='submit' class='grey' value='View' /></td>";
+                                    $output.= "</tr></table>"; 
+                                    //$search_output.= "<input type='submit' class='grey' value='View' />";
+                                    $output.= "<hr class='manager-long-hr' />";
+                                    $output.= "</form>";
+                                    $output.= "</div>";
+                                }
+                            }else{
+                                $output.= "0 results";
+                            }
+                        }else{
+                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 
+                        }                                
+                    }
+                ?>
     </head>
 
 <body>
@@ -47,66 +105,7 @@
                                 mysqli_close($conn);
                             ?>
                         </div>
-                <?php 
-                    require_once('../../model/DBConnection.php');
-                    $connObj = new DBConnection();
-                    $conn = $connObj->getConnection();
-                    if(isset($_POST["search"])){
-                        $searchbar = $_POST["searchbar"];
-                        $search_sql = "SELECT material_id, name, measuring_unit, quantity_in_stock FROM raw_material WHERE material_id LIKE '%$searchbar%' OR name LIKE '%$searchbar%' OR measuring_unit LIKE '%$searchbar%' OR quantity_in_stock LIKE '%$searchbar%'";
-                        $serach_output = "";
-                        $output = "";
-                        if($search_result = mysqli_query($conn, $search_sql)){
-                            if(mysqli_num_rows($search_result) > 0){
-                                while($search_row = mysqli_fetch_array($search_result)){
-                                    //$class = ($row["manager_approval"]=="approve"):"grey";
-                                    $search_output.= "<div class='item-data-row'>";
-                                    $search_output.= "<form method='post' action='../RouteHandler.php'>";
-                                    $search_output.= "<input type='text' hidden='true' name='framework_controller' value='raw_material/fashion_designer_view' />";
-                                    $search_output.= "<input type='text' hidden='true' name='material_id' value='".$search_row["material_id"]."' />";
-                                    $search_output.= "<span class='manager-ID-column'>".$search_row["material_id"]."</span><span>".$search_row["name"]."</span><span style='padding-left:24px;'>".$search_row["measuring_unit"]."</span>";
-                                    $search_output.= "<table align='right' style='margin-right:8px;' class='two-button-table'><tr>";
-                                    $search_output.= "<td><input type='submit' class='grey' value='View' /></td>";
-                                    $search_output.= "</tr></table>"; 
-                                    //$search_output.= "<input type='submit' class='grey' value='View' />";
-                                    $search_output.= "<hr class='manager-long-hr' />";
-                                    $search_output.= "</form>";
-                                    $search_output.= "</div>";
-                                }
-                            }else {
-                                    $search_output.= /*"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*/"No results found";
-                                    }
-                        }
-                    }else{
-                        $search_sql = "SELECT material_id, name, measuring_unit, quantity_in_stock FROM `raw_material` WHERE `manager_approval`='approve';";
-                        $serach_output = "";
-                        $output = "";
-                        if($result = mysqli_query($conn, $sql)){
-                            if(mysqli_num_rows($result) > 0){
-                                while($row = mysqli_fetch_array($result)){
-                                    //$class = ($row["manager_approval"]=="approve"):"grey";
-                                    $output.= "<div class='item-data-row'>";
-                                    $output.= "<form method='post' action='../RouteHandler.php'>";
-                                    $output.= "<input type='text' hidden='true' name='framework_controller' value='raw_material/fashion_designer_view' />";
-                                    $output.= "<input type='text' hidden='true' name='material_id' value='".$row["material_id"]."' />";
-                                    $output.= "<span class='manager-ID-column'>".$row["material_id"]."</span><span>".$row["name"]."</span><span style='padding-left:24px;'>".$row["measuring_unit"]."</span>";
-                                    $output.= "<table align='right' style='margin-right:8px;' class='two-button-table'><tr>";
-                                    $output.= "<td><input type='submit' class='grey' value='View' /></td>";
-                                    $output.= "</tr></table>"; 
-                                    //$search_output.= "<input type='submit' class='grey' value='View' />";
-                                    $output.= "<hr class='manager-long-hr' />";
-                                    $output.= "</form>";
-                                    $output.= "</div>";
-                                }
-                            }else{
-                                $output.= "0 results";
-                            }
-                        }else{
-                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-
-                        }                                
-                    }
-                ?>
+                
                     </div>
 
 
--- view/fashion_designer/view_request.php
+++ view/fashion_designer/view_request.php
@@ -6,6 +6,62 @@
         <title>View Requests</title>
         <link rel="stylesheet" type="text/css" href="../css/fashion_designer/data_form_style.css" />
         <link rel="stylesheet" type="text/css" href="../css/fashion_designer/view_list_style.css" />
+        <?php 
+            require_once('../../model/DBConnection.php');
+            $connObj = new DBConnection();
+            $conn = $connObj->getConnection();
+            if(isset($_POST["search"])){
+                $searchbar = $_POST["searchbar"];
+                $search_sql = "SELECT material_id, name, measuring_unit, quantity_in_stock FROM raw_material WHERE material_id LIKE '%$searchbar%' OR name LIKE '%$searchbar%' OR measuring_unit LIKE '%$searchbar%' OR quantity_in_stock LIKE '%$searchbar%'";
+                $serach_output = "";
+                $output = "";
+                if($search_result = mysqli_query($conn, $search_sql)){
+                    if(mysqli_num_rows($search_result) > 0){
+                        while($search_row = mysqli_fetch_array($search_result)){
+                            $search_output.= "<div class='item-data-row'>";
+                            $search_output.= "<form method='post' action='../RouteHandler.php'>";
+                            $search_output.= "<input type='text' hidden='true' name='framework_controller' value='raw_material/fashion_designer_request_view' />";
+                            $search_output.= "<input type='text' hidden='true' name='material_id' value='".$search_row["material_id"]."' />";
+                            $search_output.= "<span class='manager-ID-column'>".$search_row["material_id"]."</span><span>".$search_row["name"]."</span><span style='padding-left:24px;'>".$row["measuring_unit"]."</span>";
+                            $search_output.= "<table align='right' style='margin-right:8px;' class='two-button-table'><tr>";
+                            $search_output.= "<td><input type='submit' class='grey' value='View' /></td>";
+                            $search_output.= "</tr></table>"; 
+                            $search_output.= "<hr class='manager-long-hr' />";
+                            $search_output.= "</form>";
+                            $search_output.= "</div>";
+                    }
+                }else {
+                    $search_output.= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No results found";
+                    }
+                }
+            }else{
+                    $sql = "SELECT material_id, name, measuring_unit, quantity_in_stock FROM raw_material WHERE fashion_designer_id = ".$_SESSION["employee_id"].";";
+                    $serach_output = "";
+                    $output = "";
+                    if($result = mysqli_query($conn, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            while($row = mysqli_fetch_array($result)){
+                                $output.= "<div class='item-data-row'>";
+                                $output.= "<form method='post' action='../RouteHandler.php'>";
+                                $output.= "<input type='text' hidden='true' name='framework_controller' value='raw_material/fashion_designer_request_view' />";
+                                $output.= "<input type='text' hidden='true' name='material_id' value='".$row["material_id"]."' />";
+                                $output.= "<span class='manager-ID-column'>".$row["material_id"]."</span><span>".$row["name"]."</span><span style='padding-left:24px;'>".$row["measuring_unit"]."</span>";
+                                $output.= "<table align='right' style='margin-right:8px;' class='two-button-table'><tr>";
+                                $output.= "<td><input type='submit' class='grey' value='View' /></td>";
+                                $output.= "</tr></table>"; 
+                                $output.= "<hr class='manager-long-hr' />";
+                                $output.= "</form>";
+                                $output.= "</div>";
+                            }
+                        }else {
+                            $output.= "0 results";
+                            }
+            }else{
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+                }
+            }
+        ?>
+
     </head>
 
     <body>
@@ -26,8 +82,8 @@
                         <h2>Request</h2>
                     </center>
                     <center>
-                        <form method="post" action="" class="search-panel">    
-                            <input type="text" name="" id="" placeholder="Search" class="text-field" />
+                        <form method="post" action="view_request.php" class="search-panel">    
+                            <input type="text" name="searchbar" id="searchbar" placeholder="Search" class="text-field" />
                             <input type="submit" value="search" style="padding:3px;padding-left:10px;padding-right:10px;" /><br />
                         </form>
                     </center>
@@ -38,40 +94,20 @@
                             <b>Measuring unit</b>
                             <hr class="manager-long-hr" />
                         </div>
-                        <?php 
-                            require_once('../../model/DBConnection.php');
-                            $connObj = new DBConnection();
-                            $conn = $connObj->getConnection();
-                            $sql = "SELECT material_id, name, measuring_unit, quantity_in_stock FROM raw_material WHERE fashion_designer_id = ".$_SESSION["employee_id"].";";
-                            if($result = mysqli_query($conn, $sql)){
-                                if(mysqli_num_rows($result) > 0){
-                                    while($row = mysqli_fetch_array($result)){
-                                        echo "<div class='item-data-row'>";
-                                        echo "<form method='post' action='../RouteHandler.php'>";
-                                        echo "<input type='text' hidden='true' name='framework_controller' value='raw_material/fashion_designer_request_view' />";
-                                        echo "<input type='text' hidden='true' name='material_id' value='".$row["material_id"]."' />";
-                                        echo "<span class='manager-ID-column'>".$row["material_id"]."</span><span>".$row["name"]."</span><span style='padding-left:24px;'>".$row["measuring_unit"]."</span>";
-                                        echo "<table align='right' style='margin-right:8px;' class='two-button-table'><tr>";
-                                        echo "<td><input type='submit' class='grey' value='View' /></td>";
-                                        echo "</tr></table>"; 
-                                        //echo "<input type='submit' class='grey' value='View' />";
-                                        echo "<hr class='manager-long-hr' />";
-                                        echo "</form>";
-                                        echo "</div>";
-                                    }
-                                }else {
-                                    echo "0 results";
-                                }
-                            }else{
-                                echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-                            }
-                            mysqli_close($conn);
-                        ?>
+                        <div id="content-list">
+                            <?php 
+                                echo $search_output;
+                                echo $output;
+                                mysqli_close($conn);
+                            ?>
+                        </div>
+                        
                         
                     </div>
+                </div>
 
-
-                </div>            </div> 
+                </div>            
+            </div> 
         </div> 
 
         <?php include 'footer.php';?>
