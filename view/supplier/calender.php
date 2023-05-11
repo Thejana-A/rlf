9ef0f566e2ed91c11cadc31b5030f14fdd33bb8c
Nayomi17 <108719172+Nayomi17@users.noreply.github.com>
2023-05-11 12:39:08 +0530

    supplier commit May-11

--- view/supplier/calender.php
+++ view/supplier/calender.php
@@ -1,7 +1,6 @@
 <link rel="stylesheet" type="text/css" href="css/calender.css" />
 <script>
-    const quotation_request_dates = [];
-    var quotation_request_count = 0;
+    
     const material_order_dates = [];
     var material_order_count = 0;
 </script>
@@ -10,7 +9,7 @@ error_reporting(E_ERROR | E_PARSE);
     $today_date = date("Y-m-d");
     $first_date = date("Y-m-01", strtotime($today_date));
     $last_date = date("Y-m-t", strtotime($today_date)); 
-    $sql_quotation_request = "SELECT quotation_id, raw_material_quotation.quotation_id, employee.employee_id, employee.first_name AS merchandiser_first_name, employee.last_name AS merchandiser_last_name, employee.contact_no, employee.email, supplier.supplier_id, supplier.first_name AS supplier_first_name, supplier.last_name AS supplier_last_name, issue_date, valid_till, expected_delivery_date, request_date, valid_till FROM raw_material_quotation, employee, supplier WHERE raw_material_quotation.merchandiser_id = employee.employee_id AND raw_material_quotation.supplier_id = supplier.supplier_id AND raw_material_quotation_id = raw_material_quotation.quotation_id AND material_quotation.expected_delivery_date >= '$first_date' AND material_quotation.expected_delivery_date <= '$last_date' AND (material_quotation.request_date IS NOT NULL) AND (material_quotation.issue_date IS NULL);";
+    /*$sql_quotation_request = "SELECT quotation_id, raw_material_quotation.quotation_id, employee.employee_id, employee.first_name AS merchandiser_first_name, employee.last_name AS merchandiser_last_name, employee.contact_no, employee.email, supplier.supplier_id, supplier.first_name AS supplier_first_name, supplier.last_name AS supplier_last_name, issue_date, valid_till, expected_delivery_date, request_date, valid_till FROM raw_material_quotation, employee, supplier WHERE raw_material_quotation.merchandiser_id = employee.employee_id AND raw_material_quotation.supplier_id = supplier.supplier_id AND raw_material_quotation_id = raw_material_quotation.quotation_id AND material_quotation.expected_delivery_date >= '$first_date' AND material_quotation.expected_delivery_date <= '$last_date' AND (material_quotation.request_date IS NOT NULL) AND (material_quotation.issue_date IS NULL);";
     $result_quotation_request = mysqli_query($conn, $sql_quotation_request);
     if(mysqli_num_rows($result_quotation_request) > 0){
         while($quotation_request_row = mysqli_fetch_array($result_quotation_request)){
@@ -19,9 +18,9 @@ error_reporting(E_ERROR | E_PARSE);
                 quotation_request_count++;
             </script><?php
         }
-    }
+    } */
 
-    $sql_material_order = "SELECT raw_material_quotation.quotation_id, order_id, employee.employee_id, employee.first_name AS merchandiser_first_name, employee.last_name AS merchandiser_last_name, employee.contact_no, supplier.supplier_id, supplier.first_name AS supplier_first_name, supplier.last_name AS supplier_last_name, issue_date, valid_till, expected_delivery_date, raw_material_order.manager_approval, raw_material_order.approval_description, dispatch_date, payment, payment_date FROM raw_material_quotation, raw_material_order, employee, supplier WHERE raw_material_order.quotation_id = raw_material_quotation.quotation_id AND raw_material_quotation.merchandiser_id = employee.employee_id AND raw_material_quotation.supplier_id = supplier.supplier_id AND raw_material_quotation.expected_delivery_date >= '$first_date' AND raw_material_quotation.expected_delivery_date <= '$last_date' AND (raw_material_order.dispatch_date IS NULL) AND (raw_material_order.manager_approval != 'reject');";
+    /*$sql_material_order = "SELECT raw_material_quotation.quotation_id, order_id, employee.employee_id, employee.first_name AS merchandiser_first_name, employee.last_name AS merchandiser_last_name, employee.contact_no, supplier.supplier_id, supplier.first_name AS supplier_first_name, supplier.last_name AS supplier_last_name, issue_date, valid_till, expected_delivery_date, raw_material_order.manager_approval, raw_material_order.approval_description, dispatch_date, payment, payment_date FROM raw_material_quotation, raw_material_order, employee, supplier WHERE raw_material_order.quotation_id = raw_material_quotation.quotation_id AND raw_material_quotation.merchandiser_id = employee.employee_id AND raw_material_quotation.supplier_id = supplier.supplier_id AND raw_material_quotation.expected_delivery_date >= '$first_date' AND raw_material_quotation.expected_delivery_date <= '$last_date' AND (raw_material_order.dispatch_date IS NULL) AND (raw_material_order.manager_approval != 'reject');";
     $result_material_order = mysqli_query($conn, $sql_material_order);
     if(mysqli_num_rows($result_material_order) > 0){
         while($material_order_row = mysqli_fetch_array($result_material_order)){
@@ -30,7 +29,18 @@ error_reporting(E_ERROR | E_PARSE);
                 material_order_count++;
             </script><?php
         }
-    }
+    } */
+    
+    $sql_material_order = "SELECT raw_material_quotation.quotation_id, raw_material_order.order_id, supplier_id, expected_delivery_date, manager_approval, dispatch_date  FROM raw_material_quotation, raw_material_order WHERE raw_material_order.quotation_id = raw_material_quotation.quotation_id AND raw_material_quotation.supplier_id = ".$_SESSION["supplier_id"]." AND raw_material_quotation.expected_delivery_date >= '$first_date' AND raw_material_quotation.expected_delivery_date <= '$last_date' AND (raw_material_order.dispatch_date IS NULL) AND (raw_material_order.manager_approval != 'reject');";
+    $result_material_order = mysqli_query($conn, $sql_material_order);
+    if(mysqli_num_rows($result_material_order) > 0){
+        while($material_order_row = mysqli_fetch_array($result_material_order)){
+            ?><script>
+                material_order_dates[material_order_count] = "<?php echo explode("-",$material_order_row["expected_delivery_date"])[2]; ?>";
+                material_order_count++;
+            </script><?php
+        }
+    } 
     
 ?>
 
@@ -62,11 +72,7 @@ error_reporting(E_ERROR | E_PARSE);
                         if(numberOfDate == date.getDate()){
                             calendar += "<td class='today_cell'>"+numberOfDate+"</td>";
                         }else{
-                            if((quotation_request_dates.includes(numberOfDate.toString())==true)&&(material_order_dates.includes(numberOfDate.toString())==true)){
-                                calendar += "<td class='both_order_cell'>"+numberOfDate+"</td>";
-                            }else if(quotation_request_dates.includes(numberOfDate.toString())==true){
-                                calendar += "<td class='quotation_request_cell'>"+numberOfDate+"</td>";
-                            }else if(material_order_dates.includes(numberOfDate.toString())==true){
+                            if(material_order_dates.includes(numberOfDate.toString())==true){
                                 calendar += "<td class='material_order_cell'>"+numberOfDate+"</td>";
                             }else{
                                 calendar += "<td>"+numberOfDate+"</td>";
@@ -93,11 +99,7 @@ error_reporting(E_ERROR | E_PARSE);
                         if(numberOfDate == date.getDate()){
                             calendar += "<td class='today_cell'>"+numberOfDate+"</td>";
                         }else{
-                            if((quotation_request_dates.includes(numberOfDate.toString())==true)&&(material_order_dates.includes(numberOfDate.toString())==true)){
-                                calendar += "<td class='both_order_cell'>"+numberOfDate+"</td>";
-                            }else if(quotation_request_dates.includes(numberOfDate.toString())==true){
-                                calendar += "<td class='quotation_request_cell'>"+numberOfDate+"</td>";
-                            }else if(material_order_dates.includes(numberOfDate.toString())==true){
+                            if(material_order_dates.includes(numberOfDate.toString())==true){
                                 calendar += "<td class='material_order_cell'>"+numberOfDate+"</td>";
                             }else{
                                 calendar += "<td>"+numberOfDate+"</td>";
@@ -124,11 +126,7 @@ error_reporting(E_ERROR | E_PARSE);
                         if(numberOfDate == date.getDate()){
                             calendar += "<td class='today_cell'>"+numberOfDate+"</td>";
                         }else{
-                            if((quotation_request_dates.includes(numberOfDate.toString())==true)&&(material_order_dates.includes(numberOfDate.toString())==true)){
-                                calendar += "<td class='both_order_cell'>"+numberOfDate+"</td>";
-                            }else if(quotation_request_dates.includes(numberOfDate.toString())==true){
-                                calendar += "<td class='quotation_request_cell'>"+numberOfDate+"</td>";
-                            }else if(material_order_dates.includes(numberOfDate.toString())==true){
+                            if(material_order_dates.includes(numberOfDate.toString())==true){
                                 calendar += "<td class='material_order_cell'>"+numberOfDate+"</td>";
                             }else{
                                 calendar += "<td>"+numberOfDate+"</td>";
@@ -155,11 +153,7 @@ error_reporting(E_ERROR | E_PARSE);
                         if(numberOfDate == date.getDate()){
                             calendar += "<td class='today_cell'>"+numberOfDate+"</td>";
                         }else{
-                            if((quotation_request_dates.includes(numberOfDate.toString())==true)&&(material_order_dates.includes(numberOfDate.toString())==true)){
-                                calendar += "<td class='both_order_cell'>"+numberOfDate+"</td>";
-                            }else if(quotation_request_dates.includes(numberOfDate.toString())==true){
-                                calendar += "<td class='quotation_request_cell'>"+numberOfDate+"</td>";
-                            }else if(material_order_dates.includes(numberOfDate.toString())==true){
+                            if(material_order_dates.includes(numberOfDate.toString())==true){
                                 calendar += "<td class='material_order_cell'>"+numberOfDate+"</td>";
                             }else{
                                 calendar += "<td>"+numberOfDate+"</td>";
@@ -186,11 +180,7 @@ error_reporting(E_ERROR | E_PARSE);
                         if(numberOfDate == date.getDate()){
                             calendar += "<td class='today_cell'>"+numberOfDate+"</td>";
                         }else{
-                            if((quotation_request_dates.includes(numberOfDate.toString())==true)&&(material_order_dates.includes(numberOfDate.toString())==true)){
-                                calendar += "<td class='both_order_cell'>"+numberOfDate+"</td>";
-                            }else if(quotation_request_dates.includes(numberOfDate.toString())==true){
-                                calendar += "<td class='quotation_request_cell'>"+numberOfDate+"</td>";
-                            }else if(material_order_dates.includes(numberOfDate.toString())==true){
+                            if(material_order_dates.includes(numberOfDate.toString())==true){
                                 calendar += "<td class='material_order_cell'>"+numberOfDate+"</td>";
                             }else{
                                 calendar += "<td>"+numberOfDate+"</td>";
@@ -217,11 +207,7 @@ error_reporting(E_ERROR | E_PARSE);
                         if(numberOfDate == date.getDate()){
                             calendar += "<td class='today_cell'>"+numberOfDate+"</td>";
                         }else{
-                            if((quotation_request_dates.includes(numberOfDate.toString())==true)&&(material_order_dates.includes(numberOfDate.toString())==true)){
-                                calendar += "<td class='both_order_cell'>"+numberOfDate+"</td>";
-                            }else if(quotation_request_dates.includes(numberOfDate.toString())==true){
-                                calendar += "<td class='quotation_request_cell'>"+numberOfDate+"</td>";
-                            }else if(material_order_dates.includes(numberOfDate.toString())==true){
+                            if(material_order_dates.includes(numberOfDate.toString())==true){
                                 calendar += "<td class='material_order_cell'>"+numberOfDate+"</td>";
                             }else{
                                 calendar += "<td>"+numberOfDate+"</td>";
@@ -248,11 +234,7 @@ error_reporting(E_ERROR | E_PARSE);
                         if(numberOfDate == date.getDate()){
                             calendar += "<td class='today_cell'>"+numberOfDate+"</td>";
                         }else{
-                            if((quotation_request_dates.includes(numberOfDate.toString())==true)&&(material_order_dates.includes(numberOfDate.toString())==true)){
-                                calendar += "<td class='both_order_cell'>"+numberOfDate+"</td>";
-                            }else if(quotation_request_dates.includes(numberOfDate.toString())==true){
-                                calendar += "<td class='quotation_request_cell'>"+numberOfDate+"</td>";
-                            }else if(material_order_dates.includes(numberOfDate.toString())==true){
+                            if(material_order_dates.includes(numberOfDate.toString())==true){
                                 calendar += "<td class='material_order_cell'>"+numberOfDate+"</td>";
                             }else{
                                 calendar += "<td>"+numberOfDate+"</td>";
@@ -269,7 +251,7 @@ error_reporting(E_ERROR | E_PARSE);
             break;
     }
             
-    calendar += "<tr><td colspan='7'><center><b style='color:#f12020;background-color:#f12020;'>oo</b> Quotation <b style='color:#00ff00;background-color:#00ff00;'>oo</b> Material <b style='color:#ffff00;background-color:#ffff00;'>oo</b> Both </center></td></tr>";
+    calendar += "<tr><td colspan='7'><center><b style='color:#00ff00;background-color:#00ff00;'>oo</b> Material purchase requests </center></td></tr>";
     calendar += "</table>";
     document.write(calendar);
     //alert(costume_order_dates);
