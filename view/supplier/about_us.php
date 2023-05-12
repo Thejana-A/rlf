57911819b5ad5e6f04d2b2495e4cec3fce0c0bf0
Nayomi17 <108719172+Nayomi17@users.noreply.github.com>
2023-05-12 15:31:01 +0530

    supplier commit_May-12

--- view/supplier/about_us.php
+++ view/supplier/about_us.php
@@ -1,54 +1,59 @@
 <?php require_once 'redirect.php' ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>About Us</title>
-        <link rel="stylesheet" type="text/css" href="../supplier/css/data_form_style.css" />
-        <link rel="stylesheet" type="text/css" href="../supplier/css/view_list_style.css" />
-        <style>
-            .container {
-            position: relative;
-            font-family: Arial;
-            text-align: justify;
-            }
 
-            .text-block {
-            position: absolute;
-            bottom: 20px;
-            right: 20px;
-            background-color: #cccccc;
-            color: black;
-            padding-left: 20px;
-            padding-right: 20px;
-            opacity: 0.8;
-            }
-</style>
-    </head>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About Us</title>
+    <link rel="stylesheet" type="text/css" href="../supplier/css/data_form_style.css" />
+    <link rel="stylesheet" type="text/css" href="../supplier/css/view_list_style.css" />
+    <style>
+    .container {
+        position: relative;
+        font-family: Arial;
+        text-align: justify;
+    }
 
-    <body>
-        <?php include 'header.php';?>
+    .text-block {
+        position: absolute;
+        bottom: 20px;
+        right: 20px;
+        background-color: #cccccc;
+        color: black;
+        padding-left: 20px;
+        padding-right: 20px;
+        opacity: 0.8;
+    }
+    </style>
+</head>
 
-        <div id="page-body">
-            <?php include 'leftnav.php';?>
+<body>
+    <?php include 'header.php';?>
 
-            <div id="page-content">
-                <div id="breadcrumb">
-                    <a href="http://localhost/rlf">Welcome </a> 
-                     > About us
+    <div id="page-body">
+        <?php include 'leftnav.php';?>
+
+        <div id="page-content">
+            <div id="breadcrumb">
+                <a href="http://localhost/rlf">Welcome </a>
+                > About us
+            </div>
+            <div class="container">
+                <img src="images/about_us.jpg" alt="Nature" style="width:125%; background-repeat: no-repeat;">
+                <div class="text-block">
+                    <h4>About Us</h4>
+                    <p>RLF Apparel T-shirt manufacturing company started in 2012. Mr.Anoona Udattawa is a product
+                        development technologist who works in RLF apparel factory. He was graduate in University of
+                        moratuwa. And he has a degree in Bachelor of Design in Fashion Design & Product Development,
+                        University of Moratuwa .</p>
                 </div>
-                <div class="container">
-                    <img src="images/about_us.jpg" alt="Nature" style="width:125%; background-repeat: no-repeat;">
-                    <div class="text-block">
-                        <h4>About Us</h4>
-                        <p>RLF Apparel T-shirt manufacturing company started in 2012. Mr.Anoona Udattawa is a product development technologist who works in RLF apparel factory. He was graduate in University of moratuwa. And he has a degree in Bachelor of Design in Fashion Design & Product Development, University of  Moratuwa .</p>
-                    </div>
+            </div>
+
         </div>
-                         
-            </div> 
-        </div> 
+    </div>
 
-        <?php include 'footer.php';?>
+    <?php include 'footer.php';?>
 
-    </body> 
+</body>
+
 </html>
--- view/supplier/all_quotation_requests.php
+++ view/supplier/all_quotation_requests.php
@@ -77,7 +77,7 @@
                             $search_output.= "<input type='text' hidden='true' name='approval_description' value='".$search_row["approval_description"]."' />";
                             $search_output.= "<input type='text' hidden='true' name'supplier_id' value='".$search_row["supplier_id"]."' />";
                             $search_output.= "<input type='text' hidden='true' name'merchandiser_id' value='".$search_row["merchandiser_id"]."' />";
-                            $search_output.= "<span style='width:5%;'>".$search_row["quotation_id"]."</span><span style='width:10%;'>".$search_row["first_name"]." ".$search_row["last_name"]."</span><span style='width:12%;'>".$search_row["request_date"]."</span><span style='width:12%;'>".($search_row["issue_date"]==""?"Pending":$search_row["issue_date"])."</span><span style='width:10%;'>".($search_row["valid_till"]==""?"N/A":$search_row["valid_till"])."</span><span style='width:10%;'>".(($search_row["supplier_approval"])==""?"Pending":$search_row["supplier_approval"])."</span>";
+                            $search_output.= "<span style='width:6%;'>".$search_row["quotation_id"]."</span><span style='width:12%;'>".$search_row["first_name"]." ".$search_row["last_name"]."</span><span style='width:12%;'>".$search_row["request_date"]."</span><span style='width:12%;'>".($search_row["issue_date"]==""?"Pending":$search_row["issue_date"])."</span><span style='width:8%;'>".($search_row["valid_till"]==""?"N/A":$search_row["valid_till"])."</span><span style='width:8%;'>".(($search_row["supplier_approval"])==""?"Pending":$search_row["supplier_approval"])."</span>";
                             $search_output.= "<table align='right' style='margin-right:8px;' class='two-button-table'><tr>";
                             $search_output.= "<td><input type='submit' class='".$class."' value='View' /></td>";
                             $search_output.= "</tr></table>";
@@ -106,8 +106,8 @@
                                         $output.= "<form method='post' action='../RouteHandler.php'>";
                                         $output.= "<input type='text' hidden='true' name='framework_controller' value='raw_material_quotation/supplier_view' />";
                                         $output.= "<input type='text' hidden='true' name='quotation_id' value='".$row["quotation_id"]."' />";
-                                        $output.= "<span style='width:5%;'>".$row["quotation_id"]."</span><span class='width:10%;'>".$row["first_name"]." ".$row["last_name"]."</span><span style='padding-left:24px;width:12%;'>".$row["request_date"]."</span><span style='width:12%;'>".($row["issue_date"]==""?"Pending":$row["issue_date"])."</span><span style='width:10%;'>".($row["valid_till"]==""?"N/A":$row["valid_till"])."</span><span style='width:10%;'>".(($row["supplier_approval"])==""?"Pending":$row["supplier_approval"])."</span>";
-                                        $output.= "<table align='right' style='margin-right:14px; margin-bottom:12px' class='two-button-table'><tr>";
+                                        $output.= "<span style='width:4%;'>".$row["quotation_id"]."</span><span class='width:8%;'>".$row["first_name"]." ".$row["last_name"]."</span><span style='width:10%;'>".$row["request_date"]."</span><span style='width:12%;'>".($row["issue_date"]==""?"Pending":$row["issue_date"])."</span><span style='width:8%;'>".($row["valid_till"]==""?"N/A":$row["valid_till"])."</span><span style='width:8%;'>".(($row["supplier_approval"])==""?"Pending":$row["supplier_approval"])."</span>";
+                                        $output.= "<table align='right' style='margin-right:14px; margin-bottom:8px' class='two-button-table'><tr>";
                                         $output.= "<td><input type='submit' class='".$class."' value='View' /></td>";
                                         $output.= "</tr></table>";
                                         $output.= "<hr class='manager-long-hr' />";
@@ -177,12 +177,12 @@
 
                     <div class="item-list">
                         <div class="item-heading-row">
-                            <b style="width:7%;">Quotation ID</b>
-                            <b style="width:9%;">Merchandiser name</b>
-                            <b style="width:10%;">Request date</b>
-                            <b style="width:12%;">Issued date</b>
-                            <b style="width:12%;">Valid till</b>
-                            <b style="width:12%;">Status</b>
+                            <b style="width:6%;">Quotation ID</b>
+                            <b style="width:11%;">Merchandiser name</b>
+                            <b style="width:12%;">Request date</b>
+                            <b style="width:10%;">Issued date</b>
+                            <b style="width:10%;">Valid till</b>
+                            <b style="width:8%;">Status</b>
 
 
                             <hr class = "manager-long-hr" />
